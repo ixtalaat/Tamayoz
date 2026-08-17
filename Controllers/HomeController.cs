013424bc200295fa8d5bf59d120032bbdd44f116
Talaat Ramadan <talatramadan10@gmail.com>
2026-08-17 10:12:44 +0300

    feat(home): redesign homepage with hero stats, value pillars, disciplines and testimonials

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,8 @@ using Tamayoz.Services;
 namespace Tamayoz.Controllers;
 public class HomeController(IServiceCatalogService services) : Controller
 {
-    public async Task<IActionResult> Index() => View(await services.GetActiveAsync(3));
+    public async Task<IActionResult> Index() => View(await services.GetActiveAsync(6));
+
     public IActionResult Privacy() => View();
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
